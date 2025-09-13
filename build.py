777e9a8180f51acdbcb87ba154e9c3fe9ef8e3d9
Eyad Senosy <esnosy@proton.me>
2025-09-13 21:30:50 +0300

    Update build script

--- build.py
+++ build.py
@@ -1,8 +1,15 @@
 import subprocess as sp
 import platform
+import sys
+
+debug = False
+
+if len(sys.argv) == 2:
+    if sys.argv[1] == "--debug":
+        debug = True
 
 
-def build_exe(soruces: list[str], output: str, debug: bool):
+def build_exe(soruces: list[str], output: str):
     if platform.system() == "Windows":
         args = ["cl", "/EHsc", "/std:c++20", "/O2", "/Fe" + output + ".exe", *soruces]
         if debug:
@@ -15,5 +22,5 @@ def build_exe(soruces: list[str], output: str, debug: bool):
     sp.run(args)
 
 
-build_exe(["apps/sample_surface.cpp", "libs/read_stl.cpp"], "sample_surface", False)
-build_exe(["apps/bvh.cpp", "libs/read_stl.cpp"], "bvh", False)
+build_exe(["apps/sample_surface.cpp", "libs/read_stl.cpp"], "sample_surface")
+build_exe(["apps/bvh.cpp", "libs/read_stl.cpp"], "bvh")
