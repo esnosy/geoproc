8fe3e0e8ce6f04051fe596b69450ce8bb70e7be8
Eyad Senosy <esnosy@proton.me>
2026-04-27 17:34:57 +0300

    Reduce code repetition in `triangle.hpp`

--- libs/triangle.hpp
+++ libs/triangle.hpp
@@ -7,18 +7,12 @@ template <typename T> struct Triangle {
   Vec3<T> a, b, c;
   Vec3<T> &operator[](int i) { return (&a)[i]; }
   const Vec3<T> &operator[](int i) const { return (&a)[i]; }
-  T calc_area() const {
-    Vec3<T> ab = b - a;
-    Vec3<T> ac = c - a;
-    return 0.5 * ab.cross(ac).length();
-  }
+  T calc_area() const { return 0.5 * calc_normal_unnormalized().length(); }
   AABB<T> calc_aabb() const {
     return AABB<T>{a.min(b).min(c), a.max(b).max(c)};
   }
   Vec3<T> calc_normal() const {
-    Vec3<T> ab = b - a;
-    Vec3<T> ac = c - a;
-    return ab.cross(ac).normalized();
+    return calc_normal_unnormalized().normalized();
   }
   Vec3<T> calc_normal_unnormalized() const {
     Vec3<T> ab = b - a;
