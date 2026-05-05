5137fe0be407cea2d18e30f9d524c1695773da7d
Eyad Senosy <esnosy@proton.me>
2026-05-05 17:05:24 +0300

    Move triangulate code to its own method

--- apps/off_to_stl.cpp
+++ apps/off_to_stl.cpp
@@ -18,27 +18,6 @@ int main(int argc, char *argv[]) {
   char *output_path = argv[2];
 
   auto mesh = read_off<double>(input_path);
-  Indexed_Tri_Mesh<double> indexed_tri_mesh;
-  indexed_tri_mesh.vertices = mesh.vertices;
-  for (const auto &f : mesh.faces) {
-    if (f.size() == 3) {
-      std::array<uint32_t, 3> t;
-      t[0] = f[0];
-      t[1] = f[1];
-      t[2] = f[2];
-      indexed_tri_mesh.tris.push_back(t);
-    } else if (f.size() > 3) {
-      for (size_t i = 2; i < f.size(); i++) {
-        std::array<uint32_t, 3> t;
-        t[0] = f[0];
-        t[1] = f[i - 1];
-        t[2] = f[i];
-        indexed_tri_mesh.tris.push_back(t);
-      }
-    } else {
-      // do nothing
-    }
-  }
-
+  auto indexed_tri_mesh = mesh.triangulate();
   write_stl_binary(output_path, indexed_tri_mesh.to_tris());
 }
--- libs/indexed_ngon_mesh.hpp
+++ libs/indexed_ngon_mesh.hpp
@@ -3,9 +3,35 @@
 #include <cstdint>
 #include <vector>
 
+#include "indexed_tri_mesh.hpp"
 #include "vec3.hpp"
 
 template <typename T> struct Indexed_Ngon_Mesh {
   std::vector<Vec3<T>> vertices;
   std::vector<std::vector<uint32_t>> faces;
+
+  Indexed_Tri_Mesh<T> triangulate() {
+    Indexed_Tri_Mesh<double> result;
+    result.vertices = vertices;
+    for (const auto &f : faces) {
+      if (f.size() == 3) {
+        std::array<uint32_t, 3> t;
+        t[0] = f[0];
+        t[1] = f[1];
+        t[2] = f[2];
+        result.tris.push_back(t);
+      } else if (f.size() > 3) {
+        for (size_t i = 2; i < f.size(); i++) {
+          std::array<uint32_t, 3> t;
+          t[0] = f[0];
+          t[1] = f[i - 1];
+          t[2] = f[i];
+          result.tris.push_back(t);
+        }
+      } else {
+        // do nothing
+      }
+    }
+    return result;
+  }
 };
